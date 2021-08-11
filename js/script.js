851e5fddfed7505503ae76c8f660e8f993af0ed5
Rafael Kammer <rafa.br00@gmail.com>
2021-08-11 13:27:19 -0300

    verificaDiagonais pronto

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let tabuleiro = [
-    [[""],[""],[""],[""],[""],[""]],
-    [[""],[""],[""],[""],[""],[""]],
-    [[""],[""],[""],[""],[""],[""]],
-    [[""],[""],[""],[""],[""],[""]],
-    [[""],[""],[""],[""],[""],[""]],
-    [[""],[""],[""],[""],[""],[""]],
-    [[""],[""],[""],[""],[""],[""]]
+    [[""], [""], [""], [""], [""], [""]],
+    [[""], [""], [""], [""], [""], [""]],
+    [[""], [""], [""], [""], [""], [""]],
+    [[""], [""], [""], [""], [""], [""]],
+    [[""], [""], [""], [""], [""], [""]],
+    [[""], [""], [""], [""], [""], [""]],
+    [[""], [""], [""], [""], [""], [""]]
 ]
 
 let jogada = 'vermelho'
@@ -41,6 +41,7 @@ function startGame() {
     let insersao = geraTabuleiro(tabuleiro)
     section.append(insersao)
     button.innerText = "Reiniciar"
+    jogada = 'vermelho'
     selecionarTorres()
 }
 
@@ -67,15 +68,15 @@ function revezarJogador(event) {
                 let disco = document.createElement('div')
                 disco.classList.add('disco-vermelho')
                 torre[i].appendChild(disco)
-                
+
                 let celulaDisco = disco.parentElement
                 let colunaDisco = disco.parentElement.parentElement
                 let posicaoDisco = [Number(colunaDisco.classList[1]), Number(celulaDisco.classList[1])]
                 let classeDisco = disco.classList[0]
                 tabuleiro[posicaoDisco[0]][posicaoDisco[1]] = jogada
-                
+
                 jogada = 'preto'
-                defineVitoria(posicaoDisco,classeDisco)
+                defineVitoria(posicaoDisco, classeDisco)
                 break
 
             } else {
@@ -83,35 +84,78 @@ function revezarJogador(event) {
                 let disco = document.createElement('div')
                 disco.classList.add('disco-preto')
                 torre[i].appendChild(disco)
-                
+
                 let celulaDisco = disco.parentElement
                 let colunaDisco = disco.parentElement.parentElement
                 let posicaoDisco = [Number(colunaDisco.classList[1]), Number(celulaDisco.classList[1])]
                 let classeDisco = disco.classList[0]
                 tabuleiro[posicaoDisco[0]][posicaoDisco[1]] = jogada
-                
+
                 jogada = 'vermelho'
-                defineVitoria(posicaoDisco,classeDisco)
+                defineVitoria(posicaoDisco, classeDisco)
                 break
             }
         }
     }
 }
 
-function defineVitoria(posicao,classe) {
-    verificaHorizontal(posicao,classe)
-    verificaVertical(posicao,classe)
-    verificaDiagonalCrescente(posicao,classe)
-    verificaDiagonalDecrescente(posicao,classe)
+function defineVitoria(posicao, classe) {
+    verificaHorizontal(posicao, classe)
+    verificaVertical(posicao, classe)
+    verificaDiagonalCrescente(posicao, classe)
+    verificaDiagonalDecrescente(posicao, classe)
 }
 
-function verificaHorizontal(posicao,classe) { }
-function verificaVertical(posicao,classe) { }
+function verificaHorizontal(posicao, classe) { }
+function verificaVertical(posicao, classe) { }
 
-function verificaDiagonalCrescente(posicao,classe) {
-    console.log(posicao)
-    console.log(classe) 
-    console.table(tabuleiro)
+function verificaDiagonalCrescente(posicao, classe) {
+
+    let classeReferencia = classe.split("-")[1]
+    let conjuntos = 4
+    let coluna = posicao[0] - 3
+    let linha = posicao[1] - 3
+    
+    for (let i = 0; i < conjuntos; i++) {
+        let contagem = 0
+
+        if (coluna >= 0 && coluna <= 6 && linha >= 0 && linha <= 5) {
+            if (tabuleiro[coluna][linha] === classeReferencia) { contagem++ }
+            if (tabuleiro[coluna + 1][linha + 1] === classeReferencia) { contagem++ }
+            if (tabuleiro[coluna + 2][linha + 2] === classeReferencia) { contagem++ }
+            if (tabuleiro[coluna + 3][linha + 3] === classeReferencia) { contagem++ }
+
+        }
+        if (contagem === 4) {
+            //Chamada da função de vitória AQUI
+            console.log(classeReferencia, " venceu")
+            break
+        }
+        coluna++
+        linha++
+    }
 }
 
-function verificaDiagonalDecrescente(posicao,classe) { }
+function verificaDiagonalDecrescente(posicao, classe) {
+
+    let classeReferencia = classe.split("-")[1]
+    let conjuntos = 4
+    let coluna = posicao[0] - 3
+    let linha = posicao[1] + 3
+    for (let i = 0; i < conjuntos; i++) {
+        let contagem = 0
+        if (coluna >= 0 && coluna <= 6 && linha >= 0 && linha <= 5) {
+            if (tabuleiro[coluna][linha] === classeReferencia) { contagem++ }
+            if (tabuleiro[coluna + 1][linha - 1] === classeReferencia) { contagem++ }
+            if (tabuleiro[coluna + 2][linha - 2] === classeReferencia) { contagem++ }
+            if (tabuleiro[coluna + 3][linha - 3] === classeReferencia) { contagem++ }
+        }
+        if (contagem === 4) {
+            //Chamada da função de vitória AQUI
+            console.log(classeReferencia, " venceu")
+            break
+        }
+        coluna++
+        linha--
+    }
+}
